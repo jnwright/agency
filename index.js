ec756fed0049c941b94b590417181ea97d1361cd
Jasmine W <jazzi93@gmail.com>
2019-03-01 10:16:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $("#services").click(function(){
+        $(".services").toggleClass("show");
+    })
+    
+     $("#contact").click(function(){
+        $(".contact").toggleClass("show");
+    });
+    
+    $("#about").click(function(){
+        $(".about").toggleClass("show");
+    });
+})
